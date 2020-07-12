657ad68224d5412e16d289301e51a5b9d8180356
Derek He <derek@xtom.com>
2020-07-12 19:49:55 +1000

    initial commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Created by <a href="#" target="_blank">Duo HE</a> <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:ilheduo666@gmail.com">Contact Us</a></p>
+  </div>
+</footer>
