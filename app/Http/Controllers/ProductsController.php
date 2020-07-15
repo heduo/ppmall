417e492638e6acf795256686d2b1b7d52b95ea1d
Derek He <derek@xtom.com>
2020-07-15 12:19:42 +1000

    user ui - products filter

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -9,7 +9,45 @@ class ProductsController extends Controller
 {
     public function index(Request $request)
     {
-        $products = Product::query()->where('on_sale', true)->paginate(16);
-        return view('products.index', ['products'=>$products]);
+        // query builder
+        $builder = Product::query()->where('on_sale', true);
+
+        // if search input is not empty, search products
+        if ($search=$request->input('search', '')) {
+            $like = '%'.$search.'%';
+            // search product
+            $builder->where(function ($query) use ($like)
+            {
+                $query->where('title', 'like', $like)
+                    ->orWhere('description', 'like', $like)
+                    ->orWhereHas('skus', function ($query) use ($like)
+                    {
+                       $query->where('title', 'like', $like)
+                            ->orWhere('description', 'like', $like);
+                    });
+            });
+
+        }
+
+        // sort products
+        if ($order = $request->input('order', '')) {
+            // ends with '_asc' or '_desc'
+            if (preg_match('/^(.+)_(asc|desc)$/', $order, $matches)) {
+
+                if (in_array($matches[1], ['price', 'sold_count', 'rating'])) {
+                    $builder->orderBy($matches[1], $matches[2]);
+                }
+            }
+        }
+
+        $products = $builder->paginate(16);
+
+        return view('products.index', [
+            'products'=>$products,
+            'filters' => [
+                'search' => $search,
+                'order' => $order
+            ]
+            ]);
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,6 +6,29 @@
 <div class="col-lg-10 offset-lg-1">
 <div class="card">
   <div class="card-body">
+    <!-- Filter Start -->
+    <form action="{{ route('products.index') }}" class="search-form">
+      <div class="form-row">
+        <div class="col-md-9">
+          <div class="form-row">
+            <div class="col-auto"><input type="text" class="form-control form-control-sm" name="search" placeholder="Product Name"></div>
+            <div class="col-auto"><button class="btn btn-primary btn-sm">Search</button></div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <select name="order" class="form-control form-control-sm float-right">
+            <option value="">Sort</option>
+            <option value="price_asc">Price ASC</option>
+            <option value="price_desc">Price DESC</option>
+            <option value="sold_count_desc">Sales DESC</option>
+            <option value="sold_count_asc">Sales ASC</option>
+            <option value="rating_desc">Rating DESC</option>
+            <option value="rating_asc">Rating ASC</option>
+          </select>
+        </div>
+      </div>
+    </form>
+     <!-- Filter End -->
     <div class="row products-list">
       @foreach($products as $product)
         <div class="col-3 product-item">
@@ -23,9 +46,24 @@
         </div>
       @endforeach
     </div>
-<div class="float-right">{{ $products->render() }}</div>
+    <div class="float-right">{{ $products->appends($filters)->render() }}</div>
   </div>
 </div>
 </div>
 </div>
+@endsection
+
+@section('scriptsAfterJs')
+  <script>
+    var filters = {!! json_encode($filters) !!};
+    console.log(filters);
+    $(document).ready(function () {
+      $('.search-form input[name=search]').val(filters.search);
+      $('.search-form select[name=order]').val(filters.order);
+
+      $('.search-form select[name=order]').on('change', function () {
+        $('.search-form').submit();
+      })
+    })
+  </script>
 @endsection
