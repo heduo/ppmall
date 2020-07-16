b16029e491ef60d76a751f019af2b16c16862dc1
Derek He <derek@xtom.com>
2020-07-16 23:25:14 +1000

    install wechat pay and alipay packages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Logger;
+use Yansongda\Pay\Pay as QrCodePay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,36 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // register alipay service
+        $this->app->singleton('alipay', function ()
+        {
+            $config = config('pay.alipay');
+
+            if (app()->environment()!=='production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            }else{
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            return QrCodePay::alipay($config);
+        });
+
+        // register wechat pay service
+        $this->app->singleton('wechat_pay', function ()
+        {
+            $config = config('pay.wechat');
+
+            if (app()->environment()!=='production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            }else{
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            return QrCodePay::wechat($config);
+        
+        });
     }
 
     /**
