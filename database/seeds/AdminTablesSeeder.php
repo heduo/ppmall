a18c01554ed08422b3e2611311939cec1df94c6c
Derek He <derek@xtom.com>
2020-07-23 16:46:54 +1000

    export admin data

---
+++ database/seeds/AdminTablesSeeder.php
@@ -0,0 +1,234 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // base tables
+        Encore\Admin\Auth\Database\Menu::truncate();
+        Encore\Admin\Auth\Database\Menu::insert(
+            [
+                [
+                    "parent_id" => 0,
+                    "order" => 1,
+                    "title" => "Home",
+                    "icon" => "fa-bar-chart",
+                    "uri" => "/",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 2,
+                    "title" => "System Adminstration",
+                    "icon" => "fa-tasks",
+                    "uri" => NULL,
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 3,
+                    "title" => "Adminstrators",
+                    "icon" => "fa-users",
+                    "uri" => "auth/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 4,
+                    "title" => "Roles",
+                    "icon" => "fa-user",
+                    "uri" => "auth/roles",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 5,
+                    "title" => "Permission",
+                    "icon" => "fa-ban",
+                    "uri" => "auth/permissions",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 6,
+                    "title" => "Menu",
+                    "icon" => "fa-bars",
+                    "uri" => "auth/menu",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 2,
+                    "order" => 7,
+                    "title" => "Operation log",
+                    "icon" => "fa-history",
+                    "uri" => "auth/logs",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 0,
+                    "title" => "Users Management",
+                    "icon" => "fa-users",
+                    "uri" => "/users",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 0,
+                    "title" => "Products Management",
+                    "icon" => "fa-cubes",
+                    "uri" => "/products",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 0,
+                    "title" => "Orders Management",
+                    "icon" => "fa-money",
+                    "uri" => "/orders",
+                    "permission" => NULL
+                ],
+                [
+                    "parent_id" => 0,
+                    "order" => 0,
+                    "title" => "Coupon Codes Management",
+                    "icon" => "fa-ticket",
+                    "uri" => "/coupon_codes",
+                    "permission" => NULL
+                ]
+            ]
+        );
+
+        Encore\Admin\Auth\Database\Permission::truncate();
+        Encore\Admin\Auth\Database\Permission::insert(
+            [
+                [
+                    "name" => "All permission",
+                    "slug" => "*",
+                    "http_method" => "",
+                    "http_path" => "*"
+                ],
+                [
+                    "name" => "Dashboard",
+                    "slug" => "dashboard",
+                    "http_method" => "GET",
+                    "http_path" => "/"
+                ],
+                [
+                    "name" => "Login",
+                    "slug" => "auth.login",
+                    "http_method" => "",
+                    "http_path" => "/auth/login\r\n/auth/logout"
+                ],
+                [
+                    "name" => "User setting",
+                    "slug" => "auth.setting",
+                    "http_method" => "GET,PUT",
+                    "http_path" => "/auth/setting"
+                ],
+                [
+                    "name" => "Auth management",
+                    "slug" => "auth.management",
+                    "http_method" => "",
+                    "http_path" => "/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs"
+                ],
+                [
+                    "name" => "Users Management",
+                    "slug" => "users",
+                    "http_method" => "",
+                    "http_path" => "/users*"
+                ],
+                [
+                    "name" => "Products",
+                    "slug" => "products",
+                    "http_method" => "",
+                    "http_path" => "/products*"
+                ],
+                [
+                    "name" => "Coupon Codes",
+                    "slug" => "coupon_codes",
+                    "http_method" => "",
+                    "http_path" => "/coupon_codes*"
+                ],
+                [
+                    "name" => "Orders",
+                    "slug" => "orders",
+                    "http_method" => "",
+                    "http_path" => "/orders*"
+                ]
+            ]
+        );
+
+        Encore\Admin\Auth\Database\Role::truncate();
+        Encore\Admin\Auth\Database\Role::insert(
+            [
+                [
+                    "name" => "Administrator",
+                    "slug" => "administrator"
+                ],
+                [
+                    "name" => "Operator",
+                    "slug" => "operator"
+                ]
+            ]
+        );
+
+        // pivot tables
+        DB::table('admin_role_menu')->truncate();
+        DB::table('admin_role_menu')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "menu_id" => 2
+                ]
+            ]
+        );
+
+        DB::table('admin_role_permissions')->truncate();
+        DB::table('admin_role_permissions')->insert(
+            [
+                [
+                    "role_id" => 1,
+                    "permission_id" => 1
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 2
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 3
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 4
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 6
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 7
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 8
+                ],
+                [
+                    "role_id" => 2,
+                    "permission_id" => 9
+                ]
+            ]
+        );
+
+        // finish
+    }
+}
